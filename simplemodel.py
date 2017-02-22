bc589258d0f60832dfdf6dca2c6825e71b89ac67
ANEEZ FATIMA <aneezfatima@hotmail.com>
2017-02-21 21:58:32 -0500

    Update simplemodel.py

--- simplemodel.py
+++ simplemodel.py
@@ -1,32 +1,35 @@
 import pandas as pd
 import numpy as np
-train=pd.read_csv('train.csv')
-labels=pd.get_dummies(train.label)
-del train['label']
 import tensorflow as tf
+
+#Read train and test data
+train=pd.read_csv('train.csv')
+test=pd.read_csv('test.csv')
+
+#Extract labels as one-hot arrays
+labels=pd.get_dummies(train.label)
+#Train dataset need to have only images
+del train['label']
+
 sess = tf.InteractiveSession()
+#784 pixels and 10 classes
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
+
+#initialize variables
 sess.run(tf.global_variables_initializer())
 y = tf.matmul(x,W) + b
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-for _ in range(1000):
-        rows = np.random.choice(train.index.values, 100)
-        batch_xs=train.ix[rows]
-        batch_ys=labels.ix[rows]
-        train_step.run(feed_dict={x: batch_xs, y_: batch_ys})
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-#print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-def weight_variable(shape):
-        initial = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(initial)
 
-def bias_variable(shape):
-        initial = tf.constant(0.1, shape=shape)
-        return tf.Variable(initial)
-def conv2d(x, W):
-        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+#training
+for _ in range(1000):
+  #Batch contains 100 random images from train dataset
+	rows = np.random.choice(train.index.values, 100)
+	batch_xs=train.ix[rows]
+	batch_ys=labels.ix[rows]
+	train_step.run(feed_dict={x: batch_xs, y_: batch_ys})
+        
+#prediction for test data
