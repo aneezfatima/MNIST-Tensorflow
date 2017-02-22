e540b10e4de98f1150541069954a53f32b0005e7
ANEEZ FATIMA <aneezfatima@hotmail.com>
2017-02-21 22:03:17 -0500

    Update deepmodel.py

--- deepmodel.py
+++ deepmodel.py
@@ -21,8 +21,6 @@ b = tf.Variable(tf.zeros([10]))
 #initialize variables
 sess.run(tf.global_variables_initializer())
 y = tf.matmul(x,W) + b
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 #training
 for _ in range(1000):
@@ -31,9 +29,7 @@ for _ in range(1000):
 	batch_xs=train.ix[rows]
 	batch_ys=labels.ix[rows]
 	train_step.run(feed_dict={x: batch_xs, y_: batch_ys})
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-#print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
 def weight_variable(shape):
 	initial = tf.truncated_normal(shape, stddev=0.1)
 	return tf.Variable(initial)
@@ -46,6 +42,8 @@ def conv2d(x, W):
 
 def max_pool_2x2(x):
 	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='SAME')
+
+#First Convolutional layer
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 x_image = tf.reshape(x, [-1,28,28,1])
@@ -54,6 +52,7 @@ h_pool1 = max_pool_2x2(h_conv1)
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
+#Second convolutional layer
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
@@ -73,6 +72,7 @@ correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.global_variables_initializer())
 
+#Training
 for i in range(20000):
 	rows_50 = np.random.choice(train.index.values, 50)
 	batch_xs=train.ix[rows_50]
@@ -82,9 +82,9 @@ for i in range(20000):
 		print("step %d, training accuracy %g"%(i, train_accuracy))
 	train_step.run(feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
 
-
+#Prediction
 prediction=tf.argmax(y_conv,1)
 ans=prediction.eval(feed_dict={x:test,keep_prob: 1.0})
 a=np.asarray(ans)
-np.savetxt("foo.csv", a, delimiter=",")
+np.savetxt("ans.csv", a, delimiter=",")
 
